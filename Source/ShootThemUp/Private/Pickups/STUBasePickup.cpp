4c3dc897b4ccbedf1fb95506f4a6c0f3948fa90f
G00DLER <winnergod@vk.com>
2023-05-20 23:31:37 +0300

    Added Pickups Respawn

--- Source/ShootThemUp/Private/Pickups/STUBasePickup.cpp
+++ Source/ShootThemUp/Private/Pickups/STUBasePickup.cpp
@@ -20,6 +20,8 @@ ASTUBasePickup::ASTUBasePickup()
 void ASTUBasePickup::BeginPlay()
 {
 	Super::BeginPlay();
+
+	check(CollisionComponent);
 	
 }
 
@@ -33,6 +35,35 @@ void ASTUBasePickup::NotifyActorBeginOverlap(AActor* OtherActor)
 {
 	Super::NotifyActorBeginOverlap(OtherActor);
 
-	UE_LOG(LogBasePickup, Display, TEXT("Pickup was taken"));
-	Destroy();
+	const auto Pawn = Cast<APawn>(OtherActor);
+	if (GivePickupTo(Pawn))
+	{
+		PickupWasTaken();
+	}
+}
+
+bool ASTUBasePickup::GivePickupTo(APawn* PlayerPawn)
+{
+	return false;
+}
+
+void ASTUBasePickup::PickupWasTaken()
+{
+	CollisionComponent->SetCollisionResponseToAllChannels(ECR_Ignore);
+	if (GetRootComponent())
+	{
+		GetRootComponent()->SetVisibility(false, true);
+	}
+
+	FTimerHandle RespawnTimerHandle;
+	GetWorldTimerManager().SetTimer(RespawnTimerHandle, this, &ASTUBasePickup::Respawn, RespawnTime);
+}
+
+void ASTUBasePickup::Respawn()
+{
+	CollisionComponent->SetCollisionResponseToAllChannels(ECR_Overlap);
+	if (GetRootComponent())
+	{
+		GetRootComponent()->SetVisibility(true, true);
+	}
 }

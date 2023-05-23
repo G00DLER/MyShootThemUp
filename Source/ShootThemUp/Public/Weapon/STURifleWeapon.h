1d0c2a3a1c59952ab26fde4a25e758a80a603ef4
G00DLER <winnergod@vk.com>
2023-05-23 20:04:51 +0300

    Added Niagara System

--- Source/ShootThemUp/Public/Weapon/STURifleWeapon.h
+++ Source/ShootThemUp/Public/Weapon/STURifleWeapon.h
@@ -6,6 +6,8 @@
 #include "Weapon/STUBaseWeapon.h"
 #include "STURifleWeapon.generated.h"
 
+class USTUWeaponFXComponent;
+
 /**
  * 
  */
@@ -15,19 +17,25 @@ class SHOOTTHEMUP_API ASTURifleWeapon : public ASTUBaseWeapon
 	GENERATED_BODY()
 
 public:
+	ASTURifleWeapon();
+	
 	virtual void StartFire() override;
 	virtual void StopFire() override;
 
 protected:
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Weapon")
 	float TimeBetweenShots = 0.2f;
 
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Weapon")
 	float BulletSpread = 1.5f;
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Weapon", meta = (ClampMin = "1.0", ClampMax = "50.0"))
 	float HitWeaponDamage = 10.0f;
-	
+
+	UPROPERTY(VisibleAnywhere, Category = "VFX")
+	USTUWeaponFXComponent* WeaponFXComponent;
+
+	virtual void BeginPlay() override;
 	virtual void MakeShot() override;
 	virtual bool GetTraceData(FVector& TraceStart, FVector& TraceEnd) const override;
 

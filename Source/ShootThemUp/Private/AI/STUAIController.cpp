937ff4e43ca6e9da9376d5e53345f6ab26665881
G00DLER <winnergod@vk.com>
2023-05-29 21:40:21 +0300

    Added AI Perception Component

--- Source/ShootThemUp/Private/AI/STUAIController.cpp
+++ Source/ShootThemUp/Private/AI/STUAIController.cpp
@@ -2,4 +2,30 @@
 
 
 #include "AI/STUAIController.h"
+#include "AI/STUAICharacter.h"
+#include "Components/STUAIPerceptionComponent.h"
 
+ASTUAIController::ASTUAIController()
+{
+	STUAIPerceptionComponent = CreateDefaultSubobject<USTUAIPerceptionComponent>("STUPerceptionComponent");
+	SetPerceptionComponent(*STUAIPerceptionComponent);
+}
+
+void ASTUAIController::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+
+	const auto STUCharacter = Cast<ASTUAICharacter>(InPawn);
+	if (STUCharacter)
+	{
+		RunBehaviorTree(STUCharacter->BehaviorTreeAsset);
+	}
+}
+
+void ASTUAIController::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+	
+	const auto AimActor = STUAIPerceptionComponent->GetClosestEnemy();
+	SetFocus(AimActor);
+}

199a33fd5b9318308a5d2b73e21f2e78009e4250
G00DLER <winnergod@vk.com>
2023-06-10 11:24:40 +0300

    Fixed Camera Artifact

--- Source/ShootThemUp/Public/Player/STUPlayerCharacter.h
+++ Source/ShootThemUp/Public/Player/STUPlayerCharacter.h
@@ -8,6 +8,7 @@
 
 class USpringArmComponent;
 class UCameraComponent;
+class USphereComponent;
 
 UCLASS()
 class SHOOTTHEMUP_API ASTUPlayerCharacter : public ASTUBaseCharacter
@@ -20,24 +21,38 @@ public:
 protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
 	USpringArmComponent* SpringArmComponent;
-	
+
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
 	UCameraComponent* CameraComponent;
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+	USphereComponent* CameraCollisionComponent;
+
 	virtual void OnDeath() override;
+	virtual void BeginPlay() override;
 
 public:
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-	
+
 	virtual bool IsRunning() const override;
 
 private:
 	bool WantsToRun = false;
 	bool IsMovingForward = false;
-	
+
 	void MoveForward(float Amount);
 	void MoveRight(float Amount);
 
 	void OnStartRunning();
 	void OnStopRunning();
+
+	UFUNCTION()
+	void OnCameraCollisionBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnCameraCollisionEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex);
+	
+	void CheckCameraOverlap();
 };

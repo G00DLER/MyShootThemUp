bf31ff727617672c74956b894da52184858825fd
G00DLER <winnergod@vk.com>
2023-06-14 21:51:43 +0300

    Added Health bar for Characters

--- Source/ShootThemUp/Private/AI/STUAICharacter.cpp
+++ Source/ShootThemUp/Private/AI/STUAICharacter.cpp
@@ -5,6 +5,9 @@
 #include "GameFramework/CharacterMovementComponent.h"
 #include "Components/STUAIWeaponComponent.h"
 #include "BrainComponent.h"
+#include "Components/WidgetComponent.h"
+#include "UI/STUHealthBarWidget.h"
+#include "Components/STUHealthComponent.h"
 
 ASTUAICharacter::ASTUAICharacter(const FObjectInitializer& ObjInit)
 	: Super(ObjInit.SetDefaultSubobjectClass<USTUAIWeaponComponent>("WeaponComponent"))
@@ -18,7 +21,19 @@ ASTUAICharacter::ASTUAICharacter(const FObjectInitializer& ObjInit)
 		GetCharacterMovement()->bUseControllerDesiredRotation = true;
 		GetCharacterMovement()->RotationRate = FRotator(0.0f, 200.0f, 0.0f);
 	}
+
+	HealthWidgetComponent = CreateDefaultSubobject<UWidgetComponent>("HealthBarWidget");
+	HealthWidgetComponent->SetupAttachment(GetRootComponent());
+	HealthWidgetComponent->SetWidgetSpace(EWidgetSpace::Screen);
 }
+
+void ASTUAICharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	check(HealthWidgetComponent);
+}
+
 
 void ASTUAICharacter::OnDeath()
 {
@@ -29,4 +44,13 @@ void ASTUAICharacter::OnDeath()
 	{
 		STUController->BrainComponent->Cleanup();
 	}
+}
+
+void ASTUAICharacter::OnHealthChanged(float Health, float HealthDelta)
+{
+	Super::OnHealthChanged(Health, HealthDelta);
+
+	const auto HealthBarWidget = Cast<USTUHealthBarWidget>(HealthWidgetComponent->GetUserWidgetObject());
+	if (!HealthBarWidget) return;
+	HealthBarWidget->SetHealthPercent(HealthComponent->GetHealthPercent());
 }
--- Source/ShootThemUp/Public/AI/STUAICharacter.h
+++ Source/ShootThemUp/Public/AI/STUAICharacter.h
@@ -7,6 +7,7 @@
 #include "STUAICharacter.generated.h"
 
 class UBehaviorTree;
+class UWidgetComponent;
 
 UCLASS()
 class SHOOTTHEMUP_API ASTUAICharacter : public ASTUBaseCharacter
@@ -20,5 +21,14 @@ public:
 	UBehaviorTree* BehaviorTreeAsset;
 
 protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+	UWidgetComponent* HealthWidgetComponent;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "AI")
+	float HealthVisibilityDistance = 1000.0f;
+	
+	virtual void BeginPlay() override;
+	
 	virtual void OnDeath() override;
+	virtual void OnHealthChanged(float Health, float HealthDelta) override;
 };

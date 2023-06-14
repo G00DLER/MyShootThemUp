28374bf6d5bea54a8a931d4c32b492a13956b046
G00DLER <winnergod@vk.com>
2023-06-14 21:11:30 +0300

    Added Select Level in Menu

--- Source/ShootThemUp/Public/STUCoreTypes.h
+++ Source/ShootThemUp/Public/STUCoreTypes.h
@@ -84,6 +84,8 @@ struct FImpactData
 	FDecalData DecalData;
 };
 
+// Game
+
 USTRUCT(BlueprintType)
 struct FGameData
 {
@@ -118,3 +120,22 @@ enum class ESTUMatchState : uint8
 };
 
 DECLARE_MULTICAST_DELEGATE_OneParam(FOnMatchStateChangedSignature, ESTUMatchState);
+
+// Menu
+
+USTRUCT(BlueprintType)
+struct FLevelData
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite,  Category = "Game")
+	FName LevelName = NAME_None;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite,  Category = "Game")
+	FName LevelDisplayName = NAME_None;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite,  Category = "Game")
+	UTexture2D* LevelThumb;
+};
+
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnLevelSelectedSignature, const FLevelData&);
